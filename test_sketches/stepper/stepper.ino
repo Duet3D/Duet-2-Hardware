d889b167c689fcee9a3adae5d4aaf2104e516009
Tony <tony@think3dprint3d.com>
2013-04-18 23:52:12 +0100

    Added E1 example on expansion header pins
    To allow for testing and comparison of other stepper driver chips and
    boards three expansion header pins(PC14,PC15,PC16) are configured as
    E1_DIR, E1_STEP and E1_EN.

--- test_sketches/stepper/stepper.ino
+++ test_sketches/stepper/stepper.ino
@@ -13,7 +13,9 @@ to disable the checking for them
 For the E0_En and Z_EN pins the "UdefDuePin" libaray allows access to pins that are not defined in the Arduino
 Due software but are provided by the SAM3X8E, this library is still in development.
 
-As an example all the Extruder pins are switched with this alternative method (even though only E0_EN needs to be)
+As an example all the E0 pins are switched with this alternative method (even though only E0_EN needs to be).
+
+Three expansion header pins are assigned to E1_STEP,E1_DIR and E1_EN to demostrate communicating with off-board stepper drivers
 
 */
 #include "Arduino.h"
@@ -49,11 +51,18 @@ MCP4461 mcp;
 #define E0_EN  0//undefined pin PA5
 #define E0_STOP 4 //31
 
+#define E1_STEP 48
+#define E1_DIR 49
+#define E1_EN  47
+#define E1_STOP  Z_STOP //No E1 Stop defined
+
+
 //use to disable the axis endstop checking code so the motors step regardless
 boolean XendStopDisabled = true;
 boolean YendStopDisabled = true;
 boolean ZendStopDisabled = true;
 boolean E0endStopDisabled = true;
+boolean E1endStopDisabled = true;
 
 void setup() {
  //setup the Vrefs input to the stepper drivers
@@ -86,11 +95,17 @@ void setup() {
  pinModeUndefined(E0_EN, OUTPUT);
  pinModeUndefined(E0_STOP, INPUT);
  
+ pinMode(E1_STEP, OUTPUT);
+ pinMode(E1_DIR, OUTPUT);
+ pinMode(E1_EN, OUTPUT);
+ //pinMode(E1_STOP, INPUT);
+ 
 //disable the steppers to begin
   digitalWrite(X_EN, HIGH);
   digitalWrite(Y_EN, HIGH);
   digitalWriteUndefined(Z_EN, HIGH);
   digitalWriteUndefined(E0_EN, HIGH);
+  digitalWrite(E1_EN, HIGH);
 
 }
 
@@ -101,30 +116,35 @@ void loop() {
   digitalWrite(Y_EN, LOW);
   digitalWriteUndefined(Z_EN, LOW);
   digitalWriteUndefined(E0_EN, LOW);
+  digitalWrite(E1_EN, LOW);
   
   //CW 
   digitalWrite(X_DIR, HIGH);
   digitalWrite(Y_DIR, HIGH);
   digitalWrite(Z_DIR, HIGH);
- digitalWriteUndefined(E0_DIR, HIGH);
+  digitalWriteUndefined(E0_DIR, HIGH);
+  digitalWrite(E1_DIR, HIGH);
   
   for (int i = 1 ; i<5000 ;i++) { 
     if(digitalRead(X_STOP) == 0 || XendStopDisabled) digitalWrite(X_STEP, HIGH);
     if(digitalRead(Y_STOP) == 0 || YendStopDisabled) digitalWrite(Y_STEP, HIGH);
     if(digitalRead(Z_STOP) == 0 || ZendStopDisabled) digitalWrite(Z_STEP, HIGH);
     if(digitalRead(E0_STOP) == 0 || E0endStopDisabled)  digitalWriteUndefined(E0_STEP, HIGH);
-    delayMicroseconds(100);
+    if(digitalRead(E1_STOP) == 0 || E1endStopDisabled)  digitalWrite(E1_STEP, HIGH);
+    delayMicroseconds(90);
     if(digitalRead(X_STOP) == 0 || XendStopDisabled) digitalWrite(X_STEP, LOW);
     if(digitalRead(Y_STOP) == 0 || YendStopDisabled) digitalWrite(Y_STEP, LOW);
     if(digitalRead(Z_STOP) == 0 || ZendStopDisabled) digitalWrite(Z_STEP, LOW);
     if(digitalRead(E0_STOP) == 0 || E0endStopDisabled)  digitalWriteUndefined(E0_STEP, LOW);
-    delayMicroseconds(100);
+    if(digitalRead(E1_STOP) == 0 || E1endStopDisabled)  digitalWrite(E1_STEP, LOW);
+    delayMicroseconds(90);
   }
   //disable the steppers
   digitalWrite(X_EN, HIGH);
   digitalWrite(Y_EN, HIGH);
   digitalWriteUndefined(Z_EN, HIGH);
   digitalWriteUndefined(E0_EN, HIGH);
+  digitalWrite(E1_EN, HIGH);
   
   delay(2000);
   
@@ -133,12 +153,14 @@ void loop() {
   digitalWrite(Y_EN, LOW);
   digitalWriteUndefined(Z_EN, LOW);
   digitalWriteUndefined(E0_EN, LOW);
+  digitalWrite(E1_EN, LOW);
  
   //CCW
   digitalWrite(X_DIR, LOW);
   digitalWrite(Y_DIR, LOW);
   digitalWrite(Z_DIR, LOW);
- digitalWriteUndefined(E0_DIR, LOW);
+  digitalWriteUndefined(E0_DIR, LOW);
+  digitalWrite(E1_DIR, LOW);
   
   for (int i = 1 ; i<5000 ;i++)
   { 
@@ -146,18 +168,21 @@ void loop() {
     if(digitalRead(Y_STOP) == 0 || YendStopDisabled) digitalWrite(Y_STEP, HIGH);
     if(digitalRead(Z_STOP) == 0 || ZendStopDisabled) digitalWrite(Z_STEP, HIGH);
     if(digitalRead(E0_STOP) == 0 || E0endStopDisabled)  digitalWriteUndefined(E0_STEP, HIGH);
-    delayMicroseconds(50);
+    if(digitalRead(E1_STOP) == 0 || E1endStopDisabled)  digitalWrite(E1_STEP, HIGH);
+    delayMicroseconds(100);
     if(digitalRead(X_STOP) == 0 || XendStopDisabled) digitalWrite(X_STEP, LOW);
     if(digitalRead(Y_STOP) == 0 || YendStopDisabled) digitalWrite(Y_STEP, LOW);
     if(digitalRead(Z_STOP) == 0 || ZendStopDisabled) digitalWrite(Z_STEP, LOW);
     if(digitalRead(E0_STOP) == 0 || E0endStopDisabled)  digitalWriteUndefined(E0_STEP, LOW);
-    delayMicroseconds(50);
+    if(digitalRead(E1_STOP) == 0 || E1endStopDisabled)  digitalWrite(E1_STEP, LOW);
+    delayMicroseconds(100);
   }
   //disable the steppers
   digitalWrite(X_EN, HIGH);
   digitalWrite(Y_EN, HIGH);
   digitalWriteUndefined(Z_EN, HIGH);
   digitalWriteUndefined(E0_EN, HIGH);
+  digitalWrite(E1_EN, HIGH);
   delay(2000);
 }
 
